6d94e8a2f1701104ebafa93445dc862ab669870c
jahde <jahde@classpass.com>
2016-05-03 09:03:03 -0400

    finish results page

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,11 +1,44 @@
 var axios = require('axios');
 
-var id = 'YOUR_CLIENT_ID';
-var sec = 'YOUR_SECRET_ID';
+var id = '5410cf4508c5c0922a8c';
+var sec = 'a0fa58402f3cd7652ba53e47615fd619f98b517e';
 var param = '?client_id=' + id + '&client_secret=' + sec;
 
 function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param)
+}
+
+function getRepos(username) {
+  // fetch username repos
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100')
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function(prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  // get repos
+  // getTotalStars
+  // return object with data
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function(totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players) {
+  // return an array, after doing some fancy algorithms to determine a winner
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
 }
 
 var helpers = {
@@ -19,6 +52,14 @@ var helpers = {
     }).catch(function(err) {
       console.warn('Error in getPlayersInfo', err);
     });
+  },
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 }
 
